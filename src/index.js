1a58d3477887f315eb19ae46991cf071dc50c2de
krystian <avedol97@o2.pl>
2021-02-02 00:03:18 +0100

    button

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import styled from 'styled-components';
+import Root from './views/Root/Root';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/views/Root/Root.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from './components/Root/Root';
+
+const Root = () => (
+  <div>
+    <h1>Hello Krystian</h1>
+    <Button>Hello !</Button>
+  </div>
+);
+export default Root;
