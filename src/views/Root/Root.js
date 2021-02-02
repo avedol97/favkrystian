69258373aa15c07a12bc4a4d0dea68feda603a37
krystian <krystian.cwioro@o2.pl>
2021-02-02 22:13:12 +0100

    mainTheme

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {ThemeProvider} from 'styled-components';
 import Button from '../../components/Button/Button';
+import {theme} from '../../theme/mainTheme';
+
 
 const Root = () => (
   <div>
+    <ThemeProvider theme={theme}>
+      <>
     <h1>Hello Krystian</h1>
     <Button>CLOSE / SAVE</Button>
     <Button secondary>Remove</Button>
+      </>
+    </ThemeProvider>
   </div>
 );
 export default Root;
