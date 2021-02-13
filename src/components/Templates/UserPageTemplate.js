daf86024b07af75c4588b3456550efe4e3b840af
krystian <krystian.cwioro@o2.pl>
2021-02-13 11:44:21 +0100

    sidebar

---
+++ src/components/Templates/UserPageTemplate.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Sidebar from '../organisms/Sidebar/Sidebar';
+
+
+const UserPageTemplate = ({ children, pageType }) => (
+  <>
+    <Sidebar pageContext={pageType}/>
+    {children}
+  </>
+);
+
+UserPageTemplate.propTypes = {
+  children: PropTypes.element.isRequired,
+  pageType: PropTypes.oneOf(['notes','twitters','articles']),
+};
+
+UserPageTemplate.defaultProps = {
+  pageType:'notes',
+};
+
+export default UserPageTemplate;
--- src/views/Root/Articles.js
+++ src/views/Root/Articles.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import UserPageTemplate from '../../components/Templates/UserPageTemplate';
 
 const Articles = () =>(
+  <UserPageTemplate pageType="articles">
   <h1>Articles View</h1>
+  </UserPageTemplate>
 );
 
 export default Articles;
