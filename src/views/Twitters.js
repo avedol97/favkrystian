83b05c84b2c414c01eac8c8abef592b2b3450f78
krystian <krystian.cwioro@o2.pl>
2021-02-14 22:49:59 +0100

    configure

--- src/views/Twitters.js
+++ src/views/Twitters.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Card from '../components/molecules/Card/Card';
 import GridTemplate from '../components/Templates/GridTemplate';
 
 const mapStateToProps = ({ twitters }) => ({ twitters });
 
-const Twitters = ({twitters}) => (
+const Twitters = ({ twitters }) => (
   <GridTemplate pageType='twitters'>
-      {twitters.map(({title,content,twitterName,created,id}) => (
-        <Card
-          id={id}
-          cardType='twitters'
-          title={title}
-          content={content}
-          twitterName={twitterName}
-          created={created}
-          key={id}
-        />
-      ))}
+    {twitters.map(({ id, title, content, twitterName, created }) => (
+      <Card
+        id={id}
+        cardType='twitters'
+        title={title}
+        content={content}
+        twitterName={twitterName}
+        created={created}
+        key={id}
+      />
+    ))}
   </GridTemplate>
 );
+
+Twitters.propTypes = {
+  twitters: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    twitterName: PropTypes.string.isRequired,
+    created: PropTypes.string.isRequired,
+  })),
+};
+
+Twitters.defaultProps = {
+  twitters: [],
+};
 
 export default connect(mapStateToProps)(Twitters);
