8334ba01eb66a9f9854361570839bcb8e61a27dd
krystian <krystian.cwioro@o2.pl>
2021-02-15 12:58:44 +0100

    remove

--- src/components/Templates/MainTemplate.js
+++ src/components/Templates/MainTemplate.js
@@ -1,19 +1,51 @@
-import React from 'react';
- import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../theme/mainTheme';
 import GlobalStyle from '../../theme/GlobalStyle';
 
-const MainTemplate = ({children}) => (
-  <div>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      {children}
-    </ThemeProvider>
-  </div>
-);
+class MainTemplate extends Component {
+  constructor(props) {
+    super(props);
 
-MainTemplate.prototype = {
-  children: PropTypes.element.isRequired,
+    this.state = {
+      pageType: 'notes',
+    };
+  }
+
+  componentDidMount() {
+    this.setCurrentPage();
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    this.setCurrentPage(prevState);
+  }
+
+  setCurrentPage = (prevState='') => {
+    const pageTypes = ['twitters', 'articles', 'notes'];
+    const { location: { pathname } } = this.props;
+    const [currentPage] = pageTypes.filter(page => pathname.includes(page));
+
+    if (prevState.pageTypes !== currentPage) {
+      this.setState({ pageTypes: currentPage });
+      console.log(this.state.pageTypes);
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+      </div>
+    );
+  }
 }
-export default MainTemplate ;
+
+
+MainTemplate.propType = {
+  children: PropTypes.element.isRequired,
+};
+export default withRouter(MainTemplate);
