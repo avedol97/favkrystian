82736b6907432f48e9bc3b043e60adf28f31cfcb
krystian <krystian.cwioro@o2.pl>
2021-02-16 00:00:07 +0100

    additem

--- src/components/Templates/GridTemplate.js
+++ src/components/Templates/GridTemplate.js
@@ -1,4 +1,4 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import UserPageTemplate from './UserPageTemplate';
@@ -6,39 +6,42 @@ import Input from '../atoms/Input/Input';
 import Heading from '../atoms/Heading/Heading';
 import Paragraph from '../atoms/Paragraph/Paragraph';
 import ButtonIcon from '../atoms/ButtonIcon/ButtonIcon';
-import plusIcon from '../../assets/icons/plus.svg'
+import plusIcon from '../../assets/icons/plus.svg';
 import withContext from '../../hoc/withContext';
 import NewItemBar from '../organisms/NewItemBar/NewItemBar';
 
 
-class GridTemplate extends Component{
+class GridTemplate extends Component {
   constructor(props) {
     super(props);
 
-    this.state ={
-      var: '',
-    }
+    this.state = {
+      isNewItemBarVisible: false,
+    };
   }
 
-  render() {
-    const { children,pageContext } = this.props
+  handleNewItemBar = () => this.setState(prevState => ({ isNewItemBarVisible: !prevState.isNewItemBarVisible }));
 
-    return(
+  render() {
+    const { children, pageContext } = this.props;
+    const { isNewItemBarVisible } = this.state;
+    return (
       <UserPageTemplate>
         <StyledWrapper>
           <StyledPageHeader>
-            <Input search placeholder="Search"/>
-            <StyledHeading big as="h1">{pageContext}</StyledHeading>
+            <Input search placeholder='Search' />
+            <StyledHeading big as='h1'>{pageContext}</StyledHeading>
             <StyledParagraph>6 {pageContext}</StyledParagraph>
           </StyledPageHeader>
           <StyledGrid>{children}</StyledGrid>
-            <StyledButtonIcon icon={plusIcon} activeColor={pageContext}/>
-            <NewItemBar/>
+          <StyledButtonIcon onClick={this.handleNewItemBar} icon={plusIcon} activeColor={pageContext} />
+          <NewItemBar isVisible={isNewItemBarVisible}/>
         </StyledWrapper>
       </UserPageTemplate>
     )
   }
 }
+
 
 
 const StyledButtonIcon = styled(ButtonIcon)`
@@ -48,6 +51,7 @@ const StyledButtonIcon = styled(ButtonIcon)`
   background-color: ${({activeColor,theme}) => theme[activeColor]};
   background-size: 35%;
   border-radius: 50px;
+  z-index: 10000;
 `;
 
 const StyledWrapper = styled.div`
